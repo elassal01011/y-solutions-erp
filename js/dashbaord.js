8ab97d855b0028e857690e6697f417c4f867991e
elassal01011 <95162931+elassal01011@users.noreply.github.com>
2026-05-14 13:04:53 +0300

    Update dashbaord.js

--- js/dashbaord.js
+++ js/dashbaord.js
@@ -9,7 +9,8 @@ new Chart(ctx, {
         datasets: [{
             label: 'Revenue',
             data: [12000,25000,18000,32000,45000,60000],
-            tension:0.4
+            tension:0.4,
+            fill:true
         }]
     }
 });
