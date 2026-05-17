5c9cb15543aa60f9369da156bebd9c2af161d3f7
elassal01011 <95162931+elassal01011@users.noreply.github.com>
2026-05-17 10:13:54 +0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,3 +1,9 @@
+// Firebase SDK Import
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyARIpexHYJRjy_K2mjjro68sKi-hQEfswc",
   authDomain: "y-solutions-erp.firebaseapp.com",
@@ -8,10 +14,9 @@ const firebaseConfig = {
   measurementId: "G-YN7E8FTX3T"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
-const auth = firebase.auth();
-
-const db = firebase.firestore();
-
-console.log("Firebase Connected");
+export { auth, db };
