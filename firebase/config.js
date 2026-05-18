a2c17a9ff1f4b23d8549538d91d680330ff4e493
elassal01011 <95162931+elassal01011@users.noreply.github.com>
2026-05-18 10:25:05 +0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,22 +1,14 @@
-// Firebase SDK Import
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
-
+// Y-Solutions ERP - Firebase Initialization Config
 const firebaseConfig = {
-  apiKey: "AIzaSyARIpexHYJRjy_K2mjjro68sKi-hQEfswc",
-  authDomain: "y-solutions-erp.firebaseapp.com",
-  projectId: "y-solutions-erp",
-  storageBucket: "y-solutions-erp.firebasestorage.app",
-  messagingSenderId: "414095083537",
-  appId: "1:414095083537:web:b5dbce7b9f255fa6bfc9b0",
-  measurementId: "G-YN7E8FTX3T"
+    apiKey: "AIzaSyARIpexHYJRjy_K2mjjro68sKi-hQEfswc",
+    authDomain: "y-solutions-erp.firebaseapp.com",
+    projectId: "y-solutions-erp",
+    storageBucket: "y-solutions-erp.firebasestorage.app",
+    messagingSenderId: "414095083537",
+    appId: "1:414095083537:web:b5dbce7b9f255fa6bfc9b0",
+    measurementId: "G-YN7E8FTX3T"
 };
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
-
-export { auth, db };
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
